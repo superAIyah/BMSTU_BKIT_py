7d9f7e9e7415586efe4d0dc72ad70c739e5a16c8
Fedor <safonovfedya696@gmail.com>
2021-10-14 23:42:32 +0300

    Lab1 done

---
+++ Lab1/Lab1.py
@@ -0,0 +1,91 @@
+import sys
+import math
+
+def get_coef(index, prompt):
+    '''
+    Читаем коэффициент из командной строки или вводим с клавиатуры
+    Args:
+        index (int): Номер параметра в командной строке
+        prompt (str): Приглашение для ввода коэффицента
+    Returns:
+        float: Коэффициент квадратного уравнения
+    '''
+    try:
+        # Пробуем прочитать коэффициент из командной строки
+        coef_str = sys.argv[index]
+        coef = float(coef_str)
+    except:
+        # Вводим с клавиатуры
+        while True:
+            try:
+                print(prompt)
+                coef = float(input())
+            except:
+                continue
+            else:
+                break
+    return coef
+
+
+def get_roots(a, b, c):
+    '''
+    Вычисление корней квадратного уравнения
+    Args:
+        a (float): коэффициент А
+        b (float): коэффициент B
+        c (float): коэффициент C
+    Returns:
+        list[float]: Список корней
+    '''
+    result = []
+    D = b * b - 4 * a * c
+    if D == 0.0:
+        root = -b / (2.0 * a)
+        result.append(root)
+    elif D > 0.0:
+        sqD = math.sqrt(D)
+        root1 = (-b + sqD) / (2.0 * a)
+        root2 = (-b - sqD) / (2.0 * a)
+        result.append(root1)
+        result.append(root2)
+    result = list(filter(lambda x: x >= 0, result))
+    result_bi = []
+    for elem in result:
+        if (math.fabs(elem) == 0):
+            result_bi.append(0)
+            continue
+        x = math.sqrt(elem)
+        result_bi.append(x)
+        result_bi.append(-x)
+    return result_bi
+
+
+def main():
+    '''
+    Основная функция
+    '''
+    a = get_coef(1, 'Введите коэффициент А:')
+    b = get_coef(2, 'Введите коэффициент B:')
+    c = get_coef(3, 'Введите коэффициент C:')
+    # Вычисление корней
+    roots = get_roots(a, b, c)
+    # Вывод корней
+    len_roots = len(roots)
+    if len_roots == 0:
+        print('Нет корней')
+    elif len_roots == 1:
+        print('Один корень: {}'.format(roots[0]))
+    elif len_roots == 2:
+        print('Три корня: {} и {}'.format(roots[0], roots[1]))
+    elif len_roots == 3:
+        print('Три корня: {} и {} и {}'.format(roots[0], roots[1], roots[2]))
+    elif len_roots == 4:
+        print('4 корня: ', ', '.join(list(map(str, roots))))
+
+
+# Если сценарий запущен из командной строки
+if __name__ == "__main__":
+    main()
+
+# Пример запуска
+# qr.py 1 0 -4
