c2d74285cd865d5477460414f445684d8c8c7319
Fedor <safonovfedya696@gmail.com>
2021-10-25 00:27:22 +0300

    Cleaner code

--- Lab3/main.py
+++ Lab3/main.py
@@ -20,6 +20,7 @@ dets = [
     Detail(5, "кран", 10, 2),
 ]
 
+#Связь деталь-сотрудник
 dets_prods = [
     DetProd(2, 1),
     DetProd(4, 1),
@@ -34,68 +35,78 @@ dets_prods = [
     DetProd(5, 5),
 ]
 
-def main():
-    '''Основная функция'''
-
-    #один ко многим
-    one_to_many = [(p.name, d.name, d.price)
+#один ко многим
+ONE_TO_MANY = [(p.name, d.name, d.price)
         for p in prods
         for d in dets
         if p.id == d.id_producer]
 
-    #многие ко многим
-    many_to_many = [(d.name, p.name)
-        for p in prods
-        for d in dets
-        for dp in dets_prods
-        if dp.det_id == d.id and dp.prod_id == p.id
-    ]
+#многие ко многим
+MANY_TO_MANY = [(d.name, p.name)
+    for p in prods
+    for d in dets
+    for dp in dets_prods
+    if dp.det_id == d.id and dp.prod_id == p.id
+]
 
+def solve1():
     print("Задача 1")
     print("Вывести производителей, в которых есть слово \"завод\" и их детали")
-    correct_prods = [elem[0] for elem in one_to_many if 'завод' in elem[0]]
+    correct_prods = set(
+        [elem[0] for elem in ONE_TO_MANY if 'завод' in elem[0]])  # отбираем производителей в которых есть слово "завод"
     keys = [[] for i in range(len(correct_prods))]
-    ans = dict(zip(correct_prods, keys))
-    for p, d, _ in one_to_many:
+    ans = dict(zip(correct_prods, keys))  # словарь с заводами и пустыми списками
+    for p, d, _ in ONE_TO_MANY:
         if p in ans:
             ans[p].append(d)
     print("Ответ:")
     for elem in ans:
         print(f"{elem} : {', '.join(ans[elem])}")
-    print()
+
+def solve2():
     print("Задача 2")
     print("Вывести производителей отсортированных по средней цене их деталей."
           " Среднюю цену округлить до двух знаков после запятой")
     print("Ответ:")
-    prod_names = set(elem[0] for elem in one_to_many)
+    prod_names = set(elem[0] for elem in ONE_TO_MANY)
     mas = []
     for elem in prod_names:
-        cnt = 0
-        sum = 0
-        for p, d, price in one_to_many:
+        cnt = 0  # количество деталей завода elem
+        sum = 0  # сумма цен всех деталей
+        for p, d, price in ONE_TO_MANY:
             if p == elem:
                 sum += price
                 cnt += 1
-        mas.append((elem, sum/cnt))
+        mas.append((elem, sum / cnt))
     mas = sorted(mas, key=lambda x: x[1])
-    print(mas)
     for name, price in mas:
         print(f"{name} : {round(price, 2)}")
-    print()
+
+def solve3():
     print("Задача 3")
     print("Вывести все детали, начинающиеся на букву \"а\", "
           "а также их отделы")
     d = dict()
-    for d_name, p_name in many_to_many:
-        if d_name.startswith('а'):
-            try:
+    for d_name, p_name in MANY_TO_MANY:
+        if d_name.startswith('а'):  # название детали начинается на "а"
+            try:  # ключ уже есть
                 d[d_name].append(p_name)
-            except:
+            except:  # создаем ключ
                 d[d_name] = []
                 d[d_name].append(p_name)
     print("Ответ:")
     for elem in d:
         print(f"{elem} : {', '.join(d[elem])}")
+
+
+def main():
+    '''Основная функция'''
+    solve1()
+    print()
+    solve2()
+    print()
+    solve3()
+
 
 if __name__ == '__main__':
     main()
